80d5b4ac203e1c8a7fde926dbf37e75372b72979
xoraman <amanpatvegar@gmail.com>
2026-06-21 13:06:00 +0530

    Initial Project Setup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using skillbridge.Business.Dependency;
+using skillbridge.Data.Dependency;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDataServices(builder.Configuration);
+builder.Services.AddBusinessServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +25,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 
 app.Run();
