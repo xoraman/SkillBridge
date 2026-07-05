53c050a13803fdc11e3ad40dc4477a70ba5be13b
xoraman <amanpatvegar@gmail.com>
2026-07-05 21:24:18 +0530

    Added Business & data dependencies in program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using skillbridge.Business.Dependency;
+using skillbridge.Data.Dependency;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,20 +9,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDataServices(builder.Configuration);
+builder.Services.AddBusinessServices();
 var app = builder.Build();
 
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapControllers();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
